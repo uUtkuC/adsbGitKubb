bac5f1c67d0a5aa80659458d11b92b9b0cee2149
uUtkuC <utkuce7@gmail.com>
2024-03-20 14:13:02 +0300

    added a basic test file

---
+++ test_main.py
@@ -0,0 +1,8 @@
+from PlaneData import PlaneData
+
+
+def test_isInit_correct():
+    plane_instance = PlaneData("471f8f","WZZ4641","1710933062","27.623","40.4816","False","239.4","158.18","0")
+    assert (plane_instance.longitude == 27.623)
+
+
